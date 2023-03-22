97f54bd76f8225ac85c48bc86a5aaa3cac6e0b61
Roberto <stevkovskiroberto@gmail.com>
2023-03-22 09:55:37 +0100

    Added Swing components to ReviewSort.form

--- src/ReviewSort.java
+++ src/ReviewSort.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class ReviewSort {
+    private JPanel main;
+    private JLabel label1;
+    private JComboBox comboBox1;
+    private JLabel label2;
+    private JComboBox comboBox2;
+    private JLabel label3;
+    private JComboBox comboBox3;
+    private JLabel label4;
+    private JComboBox comboBox4;
+    private JLabel label5;
+    private JButton filterButton;
 }
